d96fc6774003663f1ddeb350967e07cbcdf96bbb
vagnermaltauro <vagnermaltauro@gmail.com>
2023-09-26 17:33:40 -0300

    feat: add components and adjust home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
-import { ModeToogle } from '@/components/mode-toggle';
+import Link from 'next/link';
+
+import { siteConfig } from '@/config/site';
+import { buttonVariants } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ModeToogle />
-    </main>
+    <section className="container grid h-screen items-center content-center justify-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-center gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-6xl">
+          Template Nextjs 13
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          Tailwind, Vitest, Testing-library, Shadcn-ui, Lucide-Icons and more!
+        </p>
+      </div>
+      <div className="flex justify-center gap-4 md:mt-4">
+        <Link
+          href="https://github.com/new?template_name=template-nextjs13&template_owner=vagnermaltauro"
+          target="_blank"
+          rel="noreferrer"
+          className={buttonVariants()}
+        >
+          Start Now
+        </Link>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={siteConfig.links.github}
+          className={buttonVariants({ variant: 'outline' })}
+        >
+          GitHub
+        </Link>
+      </div>
+    </section>
   );
 }
---
+++ config/site.ts
@@ -0,0 +1,16 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: 'Nextjs Template',
+  description: 'Template Nextjs 13 - Router App',
+  mainNav: [
+    {
+      title: 'Home',
+      href: '/',
+    },
+  ],
+  links: {
+    twitter: 'https://twitter.com/vagnermaltauro',
+    github: 'https://github.com/vagnermaltauro',
+  },
+};
